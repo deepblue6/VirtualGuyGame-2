17f31fe59a9f95c3d810c2582faaa69defe5ea3e
deepblue6 <joerosyleona@gmail.com>
2023-07-21 14:00:41 -0700

    Changed gitignore file
    changed the file with the brackeys video
    
    -------------
    
    https://www.youtube.com/watch?v=qpXxcvS-g3g

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,3 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class PlayerLife : MonoBehaviour
 {
     public Transform spawnpoint;

aacf2cc37acfad4a8645aebc6f1ace593ac0c880
deepblue6 <joerosyleona@gmail.com>
2023-08-07 11:27:09 -0700

    Double Jump and hold jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,7 +12,7 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer sprite;
     [SerializeField] private float moveSpeed = 7f; // serialize makes the value changeable in unity
     [SerializeField] private float jumpForce = 14f; // serialize makes the value changeable in unity
-    private enum MovementState { idle, running, jumping, falling}
+    private enum MovementState { idle, running, jumping, falling }
     private MovementState state = MovementState.idle;
     private BoxCollider2D coll;
     [SerializeField] private LayerMask jumpableGround;
@@ -20,12 +20,8 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode left;
     public KeyCode right;
     public KeyCode jump;
-    
-    
+    private bool doubleJump = false;
 
-
-    
-    // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -34,53 +30,58 @@ public class PlayerMovement : MonoBehaviour
         coll = GetComponent<BoxCollider2D>();
     }
 
-
-
-
-    // Update is called once per frame
-    private void Update() // void means it does not return a value, it just runs code/commands
+    private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            SceneManager.LoadScene("Level 1.0.2");
-        }
-        if(Input.GetKeyDown(KeyCode.L))
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-
         if (Input.GetKey(left))
         {
-            dirX = -1;
+            dirX = -1f; // Move left
         }
         else if (Input.GetKey(right))
         {
-            dirX = 1;
+            dirX = 1f; // Move right
         }
         else
         {
-            dirX = 0;
+            dirX = 0f; // No movement
         }
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-        if (Input.GetKeyDown(jump) && IsGrounded())
+        if (IsGrounded())
         {
-            jumpSoundEffect.Play();
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            doubleJump = false;
         }
 
-            // uses vector2 because it is a 2D game, multiplies by dirX.
-            // If it is negative, it goes left, if positive, go right.
+        if (Input.GetKeyDown(jump))
+        {
+            if (IsGrounded() || !doubleJump)
+            {
+                doubleJump = !IsGrounded();
+                jumpSoundEffect.Play();
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+        }
+        else if (Input.GetKey(jump) && rb.velocity.y > 0f)
+        {
+            // Increase the jump force while the jump button is held down, up to a maximum cap
+            rb.AddForce(Vector2.up * jumpForce * Time.deltaTime * 0.5f, ForceMode2D.Impulse);
 
+            // Cap the maximum jump height
+            if (rb.velocity.y > jumpForce)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
+        }
 
-
-            UpdateAnimationState(); // CLEAN CODE!!!!!!!!!!!
+        if (Input.GetKeyUp(jump) && rb.velocity.y > 0f)
+        {
+            // Reduce the upward velocity when the jump button is released mid-jump
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        UpdateAnimationState();
     }
-
-    
 
     private void UpdateAnimationState()
     {
-
+        
         MovementState state;
 
         if (dirX > 0f)
@@ -97,6 +98,7 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.idle;
         }
+
         if (rb.velocity.y > .1f)
         {
             state = MovementState.jumping;
@@ -105,15 +107,13 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.falling;
         }
+
         anim.SetInteger("state", (int)state);
-    } // clean code
+    }
 
     private bool IsGrounded()
-{
-    // BoxCast returns a RaycastHit2D object that can be used to check if it hit something
-    // You can use a boolean expression to check if it hit something and return the result
-    RaycastHit2D hit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
-    return hit.collider != null; // Return true if the boxcast hit something (ground), false otherwise
-}
-
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+        return hit.collider != null;
+    }
 }

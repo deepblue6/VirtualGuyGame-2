d65606ece5ee87af92cf5a16707658a9e44c236c
deepblue6 <joerosyleona@gmail.com>
2023-08-07 10:44:38 -0700

    Made Play,Menu,Quit Buttons
    NOTE: Make buttons look better

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void Menu()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Level 1.0.2");
+    }
+}
